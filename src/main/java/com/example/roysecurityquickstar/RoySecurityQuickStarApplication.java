c520a87a50a0c89d59e888ccf3060c974f15e8f6
LAPTOP-B0JU64E2\Roy <rrr111999o5@gmail.com>
2023-12-01 09:50:10 +0800

    Initial commit

---
+++ src/main/java/com/example/roysecurityquickstar/RoySecurityQuickStarApplication.java
@@ -0,0 +1,13 @@
+package com.example.roysecurityquickstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoySecurityQuickStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoySecurityQuickStarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roysecurityquickstar/RoySecurityQuickStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roysecurityquickstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoySecurityQuickStarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
